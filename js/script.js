a14ace0b6b3d7d1913372b8e279b766e1c793804
agustina cagnoni <tuti1997cagnoni@gmail.com>
2021-11-03 09:37:43 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//first button
+document.getElementById("button").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    document.getElementById("side-2").className = "flip flip-side-1";
+    document.getElementById("side-1").className = "flip flip-side-2";
+  },
+  false
+);
+document.getElementById("side-2").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    document.getElementById("side-2").className = "flip";
+    document.getElementById("side-1").className = "flip";
+  },
+  false
+);
